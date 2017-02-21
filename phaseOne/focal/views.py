7f2b57beb2a8137a9114274c027ee696e3289c70
Jared <Jarednknott@gmail.com>
2017-02-20 19:58:08 -0700

    first commit

--- phaseOne/focal/views.py
+++ phaseOne/focal/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+
+def hello(request):
+
+    today = datetime.datetime.now().date()
+
+    return render(request, "test.html", {"date": today})
+
+
+def photo(request, num):
+
+    resp = "<h> showing photo number: {}".format(num)
+
+    return HttpResponse(resp)
